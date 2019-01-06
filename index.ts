18a50a3d5c3450583666fa095ff01aae558a957c
ryuuji3 <mori.ryuuji3@gmail.com>
2019-01-06 04:04:43 +0000

    Example with request and response validation with Joi.ref

--- index.ts
+++ index.ts
@@ -1,18 +1,49 @@
 import * as Hapi from "hapi";
+import * as Joi from "joi";
 
 const server = new Hapi.Server({
     port: 3000,
     host: "localhost"
 });
 
+const requestPayloadSchema = Joi.object().keys({
+    firstName: Joi.ref("name.firstName"),
+    name: {
+        firstName: Joi.string().required(),
+    }
+});
 
+interface INamePayload {
+    firstName?: string;
+    name?: {
+        firstName: string;
+    }
+}
+
+const responseSchema = Joi.string().required();
 
 const routes: Hapi.ServerRoute[] = [
     {
         path: "/",
-        method: "GET",
+        method: "POST",
         async handler(req: Hapi.Request, h: Hapi.ResponseToolkit) {
-            return "ok";
+            const payload = req.payload as INamePayload;
+            
+            if (payload.firstName) {
+                return `Hello, ${payload.firstName}!`
+            } else if (payload.name) {
+                return `Hello, ${payload.name.firstName}`
+            } else {
+                return `Hello! I didn't catch your name, can you repeat that?`
+            }
+        },
+        options: {
+            validate: {
+                payload: requestPayloadSchema
+            },
+            response: {
+                schema: responseSchema
+            }
         }
     }
 ];
